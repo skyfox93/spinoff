5f57b13eb3849f93595e3c5d20581e0c9fa97cd4
Skylar Salerno <skylar.salerno@gmail.com>
2019-02-23 18:08:17 -0500

    change cors

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -19,7 +19,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
 
-   "/images/fallback/" + "default.png"
+   "https://spinoff-back.herokuapp.com/images/fallback/" + "default.png"
   end
 
   # Process files as they are uploaded:

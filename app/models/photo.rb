b1b6eef4493e4959db450c1105a14378b40c3ea9
Skylar Salerno <skylar.salerno@gmail.com>
2023-05-09 16:46:43 -0400

    add new user scopes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,11 +1,12 @@
 class Photo < ApplicationRecord
-  #mount_base64_uploader :old_file, OldFileUploader
   mount_base64_uploader :file, FileUploader
-
+  
   belongs_to :photo, optional:true
   belongs_to :user
   has_many :comments
   has_many :spinoffs, foreign_key: "photo_id", class_name: "Photo"
+  scope :newest, -> {order(created_at: :desc)}
+  scope :include_relations, -> {includes(:spinoffs).includes(:user)}
 
   def owner
     if self.photo
